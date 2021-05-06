c14c310d8c03cc39146e9024c49b46f6c78e16c2
Neko Shark <72129777+NekoShaaark@users.noreply.github.com>
2021-05-06 17:18:17 +0200

    Updated Explosion command
    Added:
    + Extra explosives.
    + Random number generator to determine which explosion gif is given.
    + Cooldown of 3 seconds.
    
    Removed:
    - None.
    
    Changed:
    = Made code a little bit easier to understand.

--- commands/misc/explosion.js
+++ commands/misc/explosion.js
@@ -6,7 +6,11 @@ module.exports = class explosionCommand extends Commando.Command {
             aliases: ['boom'],
             group: 'misc',
             memberName: 'explosion',
-            description: 'Explooooosion!'
+            description: 'Explooooosion!',
+            throttling: {
+                usages: 1,
+                duration: 3
+            }
         })
     }
 
@@ -14,13 +18,23 @@ module.exports = class explosionCommand extends Commando.Command {
     // runs the command
     async run(message){
 
+        //random number generator
+        var rating = Math.floor(Math.random() * 3) + 1;
+        var explosionNum
+        // will pick a random number between 3 and 25
+
+        //number determining what explosion will be shown
+        if(rating == 1){ explosionNum = ('https://cdn.discordapp.com/attachments/839881156592795659/839881353943842896/MeguminExplosion.gif') }
+        else if(rating == 2){ explosionNum = ('https://cdn.discordapp.com/attachments/839881156592795659/839881415927922698/MeguminExplosion2.gif'); }
+        else if(rating == 3){ explosionNum = ('https://cdn.discordapp.com/attachments/839881156592795659/839881439016517653/MeguminExplosion3.gif'); }
+
         const Discord = require('discord.js');
-        const archiveEmbed = new Discord.MessageEmbed()
+        const explosiveEmbed = new Discord.MessageEmbed()
         .setColor('#C20909')
         .setTitle('Explooooosion!')
-        .setImage('https://cdn.discordapp.com/attachments/834071853659586560/834074639402795038/MeguminExplosion.gif')
+        .setImage(explosionNum)
 
-        message.channel.send(archiveEmbed);
+        message.channel.send(explosiveEmbed);
 
     }
 }
