8a15d769eae7771dcba0368d9d95141f0f428417
Neko Shark <72129777+NekoShaaark@users.noreply.github.com>
2021-06-16 17:43:26 +0200

    Google API Joined the Party
    Added:
    + Googleapis Package.
    + Google Sheets Database Command (WIP).
    + Credentials.json to gitignore.
    
    Removed:
    - None.
    
    Changed:
    = None.

--- commands/dev/exe.js
+++ commands/dev/exe.js
@@ -13,9 +13,52 @@ module.exports = class exeCommand extends Commando.Command {
     // runs the command
     async run(message){
 
-        message.author.send('Testy')
-        message.channel.send('Is Testy command')
-        message.delete({ timeout: 500 })
+      // variables
+      const {google} = require('googleapis');
+      const sheets = google.sheets('v4');
 
-    }
+      //main function
+      async function main () {
+      const authClient = new google.auth.GoogleAuth({
+          keyFile: 'credentials.json',
+          scopes: 'https://www.googleapis.com/auth/spreadsheets'
+          });
+
+        const request = {
+          spreadsheetId: process.env.SPREADSHEET_ID,
+          range: 'Sheet1!A1:J1', //range of request
+          auth: authClient,
+        };
+        const name = {
+          spreadsheetId: process.env.SPREADSHEET_ID,
+          range: 'Sheet1!A2:J2',  //range of name
+          auth: authClient,
+        };
+    
+
+        try {
+          // response 1
+          const response = (await sheets.spreadsheets.values.get(request)).data.values[0];
+          //console.log(JSON.stringify(response, null, 2));
+
+          //response 2
+          const response2 = (await sheets.spreadsheets.values.get(name)).data.values[0];
+          //console.log(JSON.stringify(response2, null, 2));
+
+
+          //------embed------
+          const { MessageEmbed } = require("discord.js")
+          var embed = new MessageEmbed();
+                    
+          for (let i = 0; i < response.length; i++) 
+          { embed.addField(response[i], response2[i], false) }
+          
+          message.channel.send(embed)
+          //-----------------
+
+
+        } catch (err) { console.error(err); }
+      }
+      main();
+  }
 }
