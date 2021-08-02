ad636f22a3a6c29b67558fbf6bb917992ba33657
Neko Shark <72129777+NekoShaaark@users.noreply.github.com>
2021-08-02 19:49:54 +0200

    Updated Webhook Command
    Added:
    + Example (syntax) for help menu.
    + Function to check if an existing webhook is in the current channel (if not, create one to be used).
    + Function to check if user added <content> after command (if not, alert user).
    
    Removed:
    - Unused embed option.
    
    Changed:
    = The original message will now be deleted after the webhook copy has been sent.

--- commands/dev/webhook.js
+++ commands/dev/webhook.js
@@ -1,12 +1,13 @@
 const Commando = require('discord.js-commando')
-module.exports = class exeCommand extends Commando.Command {
+module.exports = class webhookCommand extends Commando.Command {
     constructor(client) {
         super(client, {
             name: 'webhook',
             aliases: ['wh'],
             group: 'dev',
             memberName: 'webhook',
-            description: 'Creates a webhook of the author and sends to the current channel'
+            description: 'Creates a webhook of the author and sends to the current channel',
+            examples: ['a.webhook <message>']
         })
     }
 
@@ -15,11 +16,6 @@ module.exports = class exeCommand extends Commando.Command {
     async run(message){
 
         //variables
-        const Discord = require('discord.js')
-        const embed = new Discord.MessageEmbed()
-        	.setTitle('Cooki is nom')
-        	.setColor('#0099ff')
-
         const userContent = message.content.split(' ')
         const prefix = userContent[0] + ' '
         const messageContent = message.content.slice(prefix.length)
@@ -27,15 +23,28 @@ module.exports = class exeCommand extends Commando.Command {
         const webhooks = await message.channel.fetchWebhooks()
         const webhook = webhooks.first()
 
-        
+
+        //check if webhook exists (if not create one)
+        if(!webhook){
+            message.channel.createWebhook('Sleepy Shark')
+            console.log('Created new webhook')
+            return; }
+
+
         //webhook sending
+        if(!messageContent){ message.channel.send('Pwease type message after command.') 
+        return; }
+
+        else if(messageContent){
         try {
         	await webhook.send(messageContent, {
         		username: message.author.username,
         		avatarURL: message.author.displayAvatarURL(),
-        		//embeds: [embed],
         	});
+            message.delete()
+            
         } catch (error) {
         	console.error(error) }
+    }
   }
 }
